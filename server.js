be0ac5aca15467b7711403d021a9025310f8051c
euglazer <eugenestapler@gmail.com>
2016-06-17 10:11:42 -0400

    server does nothing for now

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
 var express = require('express')
 var server = express()
 
-var i485Path = './i-485-jane.pdf'
-var g325Path = './g-325-jane-beneficiary.pdf'
-
-server.post('/process', function (req, res) {
-  res.end('meow')
-})
-
 server.listen(3000)
