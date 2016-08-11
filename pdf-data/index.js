66c8ca674949d5a2c85dd92f596e7e000ede5b46
data-doge <eugene@enspiral.com>
2016-08-11 19:37:03 -0400

    add i-765 form

--- pdf-data/index.js
+++ pdf-data/index.js
@@ -2,10 +2,12 @@ var g325BeneficiaryData = require('./g-325-foreign-born-national-data.js')
 var i485BeneficiaryData = require('./i-485-foreign-born-national-data.js')
 var g325USCitizenData = require('./g-325-us-citizen-data.js')
 var i130USCitizenData = require('./i-130-us-citizen-data.js')
+var i765ForeignBornNationalData = require('./i-765-foreign-born-national-data.js')
 
 module.exports = [
   g325BeneficiaryData,
   i485BeneficiaryData,
   g325USCitizenData,
-  i130USCitizenData
+  i130USCitizenData,
+  i765ForeignBornNationalData
 ]
--- todo.md
+++ todo.md
@@ -1,10 +1,15 @@
 ## todo
 
-- rename shit (10 min)
-  -
 - add new form to the mix
-- build auto-run script
+- fix `completed-g325a-us-citizen.pdf`
+- build auto-run test-script shit
 - finish mapping things ()
   - easy text fields
   - checkboxes (how does work -- prolly not bad)
   - fucked up text fields i-94
+
+
+## talk to geoff
+
+- what fields, if any, are optional, or not actually used.
+- will foreign folx always have an alien registration number or form i-94 number?
