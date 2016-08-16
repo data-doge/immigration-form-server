8adeda51ddb0cabf8633142d38688ea4c71391b8
data-doge <eugene@enspiral.com>
2016-08-16 08:56:34 -0400

    remove us citizen ssn for now

--- pdf-data/g-325-us-citizen-data.js
+++ pdf-data/g-325-us-citizen-data.js
@@ -5,8 +5,7 @@ module.exports = {
     return {
       'form1[0].#subform[0].FirstName[0]': commonFieldData.us_citizen_first_name,
       'form1[0].#subform[0].MiddleName[0]': commonFieldData.us_citizen_middle_name,
-      'form1[0].#subform[0].FamilyName[0]': commonFieldData.us_citizen_last_name,
-      'form1[0].#subform[0].SSN1[0]': commonFieldData.us_citizen_ssn
+      'form1[0].#subform[0].FamilyName[0]': commonFieldData.us_citizen_last_name
     }
   }
 }
